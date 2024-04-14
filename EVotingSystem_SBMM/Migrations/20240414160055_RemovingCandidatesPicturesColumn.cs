ad536f50e2d3e8be839d3ef823a294c2c171fe85
Johnn <johnnyr8821@gmail.com>
2024-04-14 18:38:01 +0100

    Final Database Changes

---
+++ EVotingSystem_SBMM/Migrations/20240414160055_RemovingCandidatesPicturesColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EVotingSystem_SBMM.Migrations
+{
+    public partial class RemovingCandidatesPicturesColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImage",
+                table: "Candidates");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImage",
+                table: "Candidates",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
